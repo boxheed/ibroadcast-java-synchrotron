c663dfb6f8a721a540713d79540b5990a1a8a36b
boxheed <boxheed@users.noreply.github.com>
2023-08-10 22:21:58 +0100

    Updated the key generator to take into account the disc numbers as ibroadcast use the disc number as part of the track number

--- src/main/groovy/com/fizzpod/ibroadcast/functions/TrackKey.groovy
+++ src/main/groovy/com/fizzpod/ibroadcast/functions/TrackKey.groovy
@@ -6,10 +6,28 @@ import org.apache.commons.io.FilenameUtils
 public class TrackKey {
 
     public static def generateKey(def track) {
-        def folder = track.folder
-        folder = FilenameUtils.separatorsToUnix(folder) 
-        folder = folder.replace("/", "")
-        track.key = "[" + folder + "] [" + track.number + "] [" + track.title + "]"
+        def folder = getFolderKey(track)
+        def number = getTrackNumber(track)
+        def title = getTrackTitle(track)
+        return "[" + folder + "] [" + number + "] [" + title + "]"
     }
+
+    public static def getFolderKey(def track) {
+        def folder = FilenameUtils.separatorsToUnix(track.folder) 
+        return folder.replace("/", "_")
+    }
+
+    public static def getTrackNumber(def track) {
+        def number = track.number
+        if(track.containsKey("disc") && track.containsKey("discTotal") && track.discTotal != "1") {
+            number = track.disc + number
+        }
+        return number
+    }
+
+    public static def getTrackTitle(def track) {
+        return track.title
+    }
+
 
 }
